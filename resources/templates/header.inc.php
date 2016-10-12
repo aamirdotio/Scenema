edb3ac1bdc30c57dc40cd2cb58c0daee48208c6a
Aamir N. Rana <a.rana@student.fontys.nl>
2016-10-12 07:16:05 +0200

    Completed header.inc.php. Except Log In links

--- resources/templates/header.inc.php
+++ resources/templates/header.inc.php
@@ -9,18 +9,66 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<!--Set Page width to size of device, and zoom level to 1-->
 	<meta name="viewport" content="width = device-width, initial-scale = 1">
-	<title><?php echo $pageTitle ?></title>
+	<title>Scenema: <?php echo $pageTitle ?></title>
 	<!--CDN Bootstrap CSS-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!--CDN Bootstrap JS-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!--CDN jQuery JS-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<!--MAIN Stylesheet-->
 	<link rel="stylesheet" href="<?php echo $siteRoot ?>/css/main.css">
-	
+	<!--CDN jQuery JS-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
 </head>
 <body>
 
-<div class="container"><!--open .container-->
+
 
 	<header>
-		
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand logoImg" href="<?php echo $siteRoot ?>/index.php"><img alt="Brand" class ="logoImg" src="<?php echo $siteRoot ?>/img/logo.png"></a>
+		      <a class="navbar-brand" href="<?php echo $siteRoot ?>/index.php">Scenema</a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="<?php echo ($pageTitle == "Home" ? "active" : "")?>"><a href="<?php echo $siteRoot ?>/index.php">Home <?php echo ($pageTitle == "Home" ? '<span class="sr-only">(current)</span>' : "")?></a></li>
+		        <li class="<?php echo ($pageTitle == "Movies" ? "active" : "")?>"><a href="<?php echo $siteRoot ?>/pages/movies.php">Movies<?php echo ($pageTitle == "Movies" ? '<span class="sr-only">(current)</span>' : "")?></a></li>
+		        <li class="<?php echo ($pageTitle == "Offers" ? "active" : "")?>"><a href="<?php echo $siteRoot ?>/pages/offers.php">Offers<?php echo ($pageTitle == "Offers" ? '<span class="sr-only">(current)</span>' : "")?></a></li>
+		        <li class="<?php echo ($pageTitle == "S-Squad" ? "active" : "")?>"><a href="<?php echo $siteRoot ?>/pages/ssquad.php">S-Squad<?php echo ($pageTitle == "S-Squad" ? '<span class="sr-only">(current)</span>' : "")?></a></li>
+		        <li class="<?php echo ($pageTitle == "About" ? "active" : "")?>"><a href="<?php echo $siteRoot ?>/pages/about.php">About<?php echo ($pageTitle == "About" ? '<span class="sr-only">(current)</span>' : "")?></a></li>
+		        
+		      </ul>
+		      
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Link</a></li>
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li role="separator" class="divider"></li>
+		            <li><a href="#">Separated link</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	
 	</header>
+
+	<div class="container"><!--open .container-->
